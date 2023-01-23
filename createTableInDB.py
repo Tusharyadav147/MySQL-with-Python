3ccb9dfee1c9b2e96f8a2113f4ea3e2c064f4c3b
Tushar Sonp` <82706499+Tusharyadav147@users.noreply.github.com>
2023-01-23 13:29:37 +0530

    Update createTableInDB.py

--- createTableInDB.py
+++ createTableInDB.py
@@ -3,7 +3,7 @@ import mysql.connector as connection
 try:
     mybd = connection.connect(host="localhost", database = "register", user = "root", password = "Tushar0420&", use_pure=True)
     print(mybd.is_connected()) #check if connection is established
-    query  = "select * from reg2;"
+    query  = "create table if not exists table_name (column_name datatype, column_name datatype);"
     cur = mybd.cursor()
     cur.execute(query)
     for i in cur.fetchall():     #fetchall fet the data
